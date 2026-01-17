b7467c2817326a2d42927efc13bcf9ab65ce8b69
blackbloww <baotx12345@gmail.com>
2026-01-17 10:35:43 +0700

    fix htacss

--- admin/store_customer.php
+++ admin/store_customer.php
@@ -2,7 +2,22 @@
 // store_customer.php
 
 // 1. Kết nối database
-include __DIR__ . '/config.php'; // hoặc file config kết nối DB của bạn
+    $DB_HOST = "localhost";
+    $DB_USER = "xs191273_baobao";
+    $DB_PASS = "dH(|0(7o3LO9";
+    $DB_NAME = "xs191273_baobao";
+
+    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+    if ($conn->connect_error) {
+        die("Kết nối thất bại: " . $conn->connect_error);
+    }
+
+    // if ($conn) {
+    //     echo "Kết nối OK!";
+    // }
+
+    $conn->set_charset("utf8mb4");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // 2. Lấy dữ liệu từ form, dùng filter để bảo vệ
--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
-    include __DIR__ . "/admin/config.php";
+    $DB_HOST = "localhost";
+    $DB_USER = "xs191273_baobao";
+    $DB_PASS = "dH(|0(7o3LO9";
+    $DB_NAME = "xs191273_baobao";
 
+    $conn = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+    if ($conn->connect_error) {
+        die("Kết nối thất bại: " . $conn->connect_error);
+    }
+
+    // if ($conn) {
+    //     echo "Kết nối OK!";
+    // }
+
+    $conn->set_charset("utf8mb4");
+    
     $id = $_GET['id'] ?? 1;
 
     if ($id && is_numeric($id)) {
